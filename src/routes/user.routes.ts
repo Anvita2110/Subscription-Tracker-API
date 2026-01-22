f3252a9320b03c14ff0f16ceeb64c1ce95611128
Anvita Praveen <anvita.2103@gmail.com>
2026-01-22 23:27:38 +0800

    add routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => {
+    res.send({title: "Get All Users"})
+})
+
+userRouter.post("/", (req, res) => {
+    res.send({title: "Create User"})
+})
+
+userRouter.get("/:id", (req, res) => {
+    res.send({title: "Get a Particular User"})
+})
+
+userRouter.put("/:id", (req, res) => {
+    res.send({title: "Update a Particular User"})
+})
+
+userRouter.delete("/:id", (req, res) => {
+    res.send({title: "Delete a Particular User"})
+})
+
+export default userRouter
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from "express"
-import dotenv from "dotenv"
-
-dotenv.config()
+import {config} from "dotenv"
+import authRouter from "./routes/auth.routes"
+import userRouter from "./routes/user.routes"
+import subscriptionRouter from "./routes/subscription.routes"
+config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
